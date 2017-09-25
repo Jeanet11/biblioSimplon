edf1b6124e7ef6604a29646d9b104763d6a41b1b
Jeanne <jeanne.tiramani@hotmail.fr>
2017-09-25 13:57:46 +0200

    page ok avec date

--- pages/detail_article.php
+++ pages/detail_article.php
@@ -3,6 +3,7 @@
 $id = $_GET['id'];
 $buttons ='';
 $buttons2 = '';
+
 if(isset($_SESSION['login'])){
     $buttons='<a href="?p=modif&id='.$id.'"><i class="glyphicon glyphicon-pencil"></i></a>';
     $buttons2='<div class="col-md-1"><a href="?p=suppr&id='.$id.'"><i class=" glyphicon glyphicon-trash"></i></a></div>';
@@ -26,9 +27,8 @@ if(!empty($_POST)) {
 
     $pseudo = htmlspecialchars($_POST['pseudo']);
     $com = htmlspecialchars($_POST['com']);
-
-    $sql_ajout_com = sprintf("INSERT INTO com_commentaire(com_pseudo, com_content, com_art_oid)
-        VALUES ('%s' , '%s' , %d)", $pseudo, $com, $id) ;
+    $sql_ajout_com = sprintf("INSERT INTO com_commentaire(com_pseudo, com_content, com_art_oid, com_date)
+        VALUES ('%s' , '%s' , %d, '%s')", $pseudo, $com, $id,date("d-m-Y")) ;
 
    
      $req=$bdd->exec($sql_ajout_com);
@@ -37,7 +37,6 @@ if(!empty($_POST)) {
     echo $e->getMessage(), "\n";
 }
   
-
 }
 
 ?>
@@ -46,25 +45,24 @@ if(!empty($_POST)) {
 <section class="clearfix container" >
     <div class="row">
         <h2 class="col-md-12"><?= $donnees['art_titre'] ?></h2>
-       
-      
-     </div>
+
+
+    </div>
 
     <div class="row">
-       <div class="col-md-9"><h4><?= $donnees['gnr_libele'] ?></h4></div>
-        <div class ='col-md-offset-2 col-md-1'><?= $buttons.$buttons2 ?></div>              
-   </div>
+     <div class="col-md-9"><h4><?= $donnees['gnr_libele'] ?></h4></div>
+     <div class ='col-md-offset-2 col-md-1'><?= $buttons.$buttons2 ?></div>              
+ </div>
 
+ <hr>
 
-        <hr>
-    
-    <div  id="content"><?=$donnees['art_content'] ?></div>
-    <hr>
-    <ul class="clearfix list-unstyled list_inline">
-        <li class='pull-right'><strong><?= $donnees['art_auteur'] ?></strong></li>
-        <li></li>
-         <li class="pull-right"><?= $donnees['art_date'] ?></li>
-    </ul>
+ <div id="content"><?=$donnees['art_content'] ?></div>
+ <hr>
+ <ul class="row list-unstyled list_inline">
+    <li class='col-md-2 col-md-offset-8'><strong><?= $donnees['art_auteur'] ?></strong></li>
+
+    <li class="col-md-2"><?= $donnees['art_date'] ?></li>
+</ul>
 </div>
 <div class="col-md-1 "><button class="pull-right btn btn-primary" type="submit" >Télécharger</button></div>
 </section >
@@ -80,24 +78,27 @@ $reponse->closeCursor();
 $reponse2 = $bdd->query("SELECT * FROM com_commentaire WHERE com_art_oid = $id ORDER BY com_date DESC");
 ?>
 
-<div class="container well">
+<div class="container well pre-scrollable">
+
     <?php
     while($donnees = $reponse2->fetch())
     {
         ?>
-      <div class="row list-unstyled comments  ">
-            <div class="col-md-2 comment" ><p><strong><?= $donnees['com_pseudo'] ?></strong></p></div>
-            <div class="col-md-9 comment"><p><em><?= $donnees['com_content'] ?></em></p></div>
-            <div class="col-md-1 col-md-offset comment"><p><?= $donnees['com_date'] ?></p></div>
+      <div class="row list-unstyled   ">
+            <div class="col-md-2 " ><p><strong><?= $donnees['com_pseudo'] ?></strong></p></div>
+            <div class="col-md-8 "><p><em><?= $donnees['com_content'] ?></em></p></div>
+            <div class="col-md-2  "><p><?= $donnees['com_date'] ?></p></div>
     
     </div>
-    
+   
 <?php
 }
 
 $reponse2->closeCursor();
 
 ?>
+
+
 <br>
 
 
@@ -110,25 +111,34 @@ $reponse2->closeCursor();
             <label class="sr-only" for="pseudo">Pseudo</label>
             <input type="text" class="form-control" id="pseudo" placeholder="pseudo" name="pseudo" maxlength="50">
         </div>
- 
-         
+
+
         <div class="col-md-5 form-group">
             <label class="sr-only" for="com">Commentaire</label>
             <textarea class="form-control" id="com" placeholder="Commentaire" name="com" maxlength="255"></textarea>
         </div>
- 
+
+        <div class="col-md-0 form-group ">
+
+              <?php
+              $date = 
+              $heure = date("H:i");
+              // echo($date." ".$heure);
+              ?>  
+       </div>
+
         <div class="col-md-2 form-group ">
             <button type="submit" class="btn btn-primary">Envoyer</button>
         </div>
- 
-    </form>
+
+        
+
+
+
+  </form>
 </div>
 
 </section>
-
-<?php
-    if (!empty($succes)){
-        echo "<h2>".$succes."</h2>";
-        $succes = "";
-    }
-?>
+<script type='text/javascript' src='node_modules/jquery/dist/jquery.js'></script>
+<script type='text/javascript' src='node_modules/markdown/lib/markdown.js'></script>
+<script type='text/javascript' src='assets/js/consult_article.js'></script>
