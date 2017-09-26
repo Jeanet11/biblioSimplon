7b9e634581debe7f4775177fb69f6f7696c70123
Jeanne <jeanne.tiramani@hotmail.fr>
2017-09-26 12:03:25 +0200

    paginaton commentaire

---
+++ assets/js/gestionCom.js
@@ -0,0 +1,8 @@
+$('document').ready(function(){
+	console.log('ok');
+    var listee = new List('idlist',{
+        valueNames: ['name'],
+        page : 5,
+        pagination: true
+    })
+});
--- pages/gestion_commentaire.php
+++ pages/gestion_commentaire.php
@@ -1,28 +1,31 @@
 <?php
-    if(empty($_SESSION['login'])){
-        header('Location: ?p=home');
-    }
-    $reponse2 = $bdd->query("SELECT * FROM com_commentaire, art_article where com_art_oid = art_oid order by art_oid")->fetchAll();
+if(empty($_SESSION['login'])){
+	header('Location: ?p=home');
+}
+$reponse2 = $bdd->query("SELECT * FROM com_commentaire, art_article where com_art_oid = art_oid order by art_oid")->fetchAll();
 ?>
 
-
-<ul class='list-unstyled'>
-	<?php foreach ($reponse2 as $key => $value): ?>
-	<li>
-		<ul class='list-unstyled'>
-			<li>Pseudo :<?= $value['com_pseudo'] ?></li>
-			<li>Commentaire :<?= $value['com_content'] ?></li>
-			<li>Date :<?= $value['com_date'] ?></li>
-			<li>Article concerné :<?= $value['art_titre']?></li>
+<div id ='idlist'>
+<ul class='list-unstyled list'>
+		<?php foreach ($reponse2 as $key => $value): ?>
 			<li>
-				<?= '<a href="?p=supprCom&id='.$value['com_oid'].'"><i class=" glyphicon glyphicon-trash"></i></a>' ?>
+				<ul class='list-unstyled name'>
+					<li>Pseudo :<?= $value['com_pseudo'] ?></li>
+					<li>Commentaire :<?= $value['com_content'] ?></li>
+					<li>Date :<?= $value['com_date'] ?></li>
+					<li>Article concerné :<?= $value['art_titre']?></li>
+					<li>
+						<?= '<a href="?p=supprCom&id='.$value['com_oid'].'"><i class=" glyphicon glyphicon-trash"></i></a>' ?>
+					</li>
+					<hr>
+				</ul>
 
 			</li>
-		</ul>
-	
-	</li>
-	<hr></hr>		
-	<?php endforeach;?>
-
-
-</ul>
+				
+		<?php endforeach;?>
+	</ul>
+	<ul class="pagination"></ul>
+</div>
+<script type='text/javascript' src='node_modules/jquery/dist/jquery.js'></script>
+<script src="node_modules/list.js/dist/list.js"></script>
+<script src="assets/js/gestionCom.js"></script>
