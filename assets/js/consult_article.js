261f0c9162e1620146e8d5ef2f0b40005dd3643c
Jeanne <jeanne.tiramani@hotmail.fr>
2017-09-26 14:38:47 +0200

    ajout téléchargement de l'article en markdown

--- assets/js/consult_article.js
+++ assets/js/consult_article.js
@@ -1,4 +1,22 @@
+
 $('document').ready(function(){
+
 	var test= $('#content').text();
 	$('#content').html(markdown.toHTML(test));
+	console.log('ok');
+
+	
+
+	$('#down').click(function(){
+		console.log('ok');
+			var content = test;
+
+			var filename = $('#title').text()+'.md';
+
+			var blob = new Blob([content], {
+				type: "text/plain;charset=utf-8"
+			});
+
+			saveAs(blob, filename);
+	})
 })
